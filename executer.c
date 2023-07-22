8623e48f53e3cea0fedd87d44f6bf54ac9913046
cyrusnderitu <cyrusnderitu.k@gmail.com>
2023-07-22 17:49:47 +0300

    Concating path with command

--- executer.c
+++ executer.c
@@ -8,13 +8,26 @@
 void _execute(char* args[]) 
 {
     pid_t child;
-    child = fork();
+    char PATH[100] = "/bin/";
+
+    _strcat(PATH, arr[0]);
+	if (stat(PATH, &st) != 0)
+    {
+		perror("could not find file");
+    }
+    else
+	{
+		args[0] = PATH;
+	    child = fork();
+	}
     if (child == 0) {
-        if (execve(args[0], args, NULL) == -1) {
+        if (execve(args[0], args, NULL) == -1) 
+        {
             perror("Command execution failed");
         }
         exit(EXIT_FAILURE);
-    } else if (child < 0) {
+    } else if (child < 0) 
+    {
         perror("Fork error");
     } else {
         wait(NULL);
