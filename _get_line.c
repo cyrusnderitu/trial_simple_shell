310e340a9863cb003e9fab102b0d4512b895484a
Cyrus_Nderitu <65945935+cyrusnderitu@users.noreply.github.com>
2023-07-22 15:18:15 +0300

    _get_line.c creation for fetching input from user

---
+++ _get_line.c
@@ -0,0 +1,22 @@
+#include "shell.h"
+
+/**
+ * _getline - Fetch input from user by getline
+ * Return: Input info.
+ */
+char *_getline(void)
+{
+	char *lineptr = NULL;
+	size_t len = 0;
+
+	if (isatty(STDIN_FILENO))
+		write(STDOUT_FILENO, "$S-shell ", 8);
+
+	if (getline(&lineptr, &len, stdin) == -1)
+	{
+		free(lineptr);
+		return (NULL);
+	}
+
+	return (lineptr);
+}
