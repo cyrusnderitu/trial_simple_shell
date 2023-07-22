c24627cc3072a5c5c0329a12cba25e2e8ffde5e1
cyrusnderitu <cyrusnderitu.k@gmail.com>
2023-07-22 11:46:09 +0300

    first commit

---
+++ shell.c
@@ -0,0 +1,69 @@
+#include "shell.h"
+
+/**
+ * main - Main function to initiate shell
+ * Return: 0 on success
+*/
+
+int main(void)
+{
+	char *commands = NULL, **input_list = NULL;
+	pid_t child;
+	char delim[] = " \n", PATH[100] = "usr/bin/";
+	int status;
+	int i = 0;
+	struct stat st;
+
+	while (1)
+	{
+		commands = _getline();
+		if (commands)
+		{
+			input_list = _strtok(commands, " ");
+			if (!input_list)
+			{
+				free(commands);
+				continue;
+			}
+			if ((!_strcmp(input_list[0], "exit")) && input_list[1] == NULL)
+			{
+				free(input_list);
+				break;
+			}
+			else
+			{
+				_strcat(PATH, input_list[0])
+				if (stat(PATH, &st) != 0)
+					perror("Could not find file");
+				else
+				{
+					input_list[0] = PATH;
+					child = fork();
+				}
+				if (pid == 0)
+				{
+					execve(input_list[0], input_list, NULL);
+					exit(1);
+				}
+				else if (pid > 0)
+				{
+					wait(&status);
+				}
+				else
+				{
+					print_error("fork failed");
+					exit(1);
+				}
+			}
+			free(input_list);
+		}
+		else
+		{
+			if (isatty(STDIN_FILENO))
+				write(STDOUT_FILENO, "\n", 1);
+			exit(_exit);
+		}
+		free(commands);
+	}
+	return (_exit);
+}
---
+++ shell.h
@@ -0,0 +1,21 @@
+#ifndef SHELL_H
+#define SHELL_H
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+
+void print_error(char *msg);
+char *_getline(void);
+char *_strtok(char *str, const char *delim);
+char *_strcpy(char *dest, char *src);
+int  _strcmp(char *str1, char *str2);
+char *_strcat(char *dest, char *src);
+int _strlen(char *s);
+char *_strtok(char *str, const char *delim);
+char *_strdup(const char *str);
+#endif
